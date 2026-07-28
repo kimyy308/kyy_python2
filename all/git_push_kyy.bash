2859f22198f6bb4e2e6d6989c42be8dbdb4cc86e
kimyy308 <kimyy308@snu.ac.kr>
2026-07-29 03:54:10 +0900

    20260729 03:54:10

--- all/git_push_kyy.bash
+++ all/git_push_kyy.bash
@@ -1,18 +1,29 @@
 #!/bin/bash
-
-# You have to run git_pull.bash first
-# Changing or adding files should be done after git_pull.bash
-# Run git_push.bash after Changing or adding files 
+set -e
 
 account='kimyy308'
-reponame='origin' # Bookmark for the remote repository. This should be the same as the name determined by git_pull.bash
-branch='main' # Branch name you want to push
-#file2upload=$1 # File or direcotry you want to push
-file2upload=all # File or direcotry you want to push
+repo='kyy_python2'
+reponame='origin'
+branch='main'
+file2upload='all'
 
-git add --ignore-removal ${file2upload} --verbose
-time=`date "+%Y%m%d %H:%M:%S"`
-git commit -m "${time}"
-git checkout -b ${branch}
+git config user.name "${account}"
+git config user.email "${account}@snu.ac.kr"
 
+git remote set-url ${reponame} https://${account}@github.com/${account}/${repo}.git
+
+git checkout ${branch}
+
+# Add only the "all" directory
+git add -A -- ${file2upload}
+
+# Commit only if there are changes in all/
+if git diff --cached --quiet; then
+    echo "No changes in ${file2upload} to commit."
+else
+    time_now=`date "+%Y%m%d %H:%M:%S"`
+    git commit -m "${time_now}"
+fi
+
+# Push local main to remote main
 git push -u ${reponame} ${branch}
